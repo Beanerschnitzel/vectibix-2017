fce582952e00d7e567007075694543fd64c07f76
ElTigreBoyer <beanerschnitzel@gmail.com>
2017-06-03 09:33:03 -0600

    structure

--- js/front-end/notifyMainToggle.js
+++ js/front-end/notifyMainToggle.js
@@ -1,23 +1,93 @@
-//nav bar toggles
+$(document).ready(function() {
+   $('.notifyMain-copy').truncateText({
+      length: 200,
+      toggleText: "Read More"
+   });
+});
+
+
+
+(function($) {
+   $.fn.truncateText = function(options) {
+      var defaults = {
+         length: 249,
+         toggleText: "Show More"
+      };
+      var options = $.extend({}, defaults, options);
+
+      var $containers = this;
+      var subLength = options.length;
+      var hiddenSubStart = subLength + 1;
+      var toggleText = options.toggleText;
+
+      $containers.each(function() {
+         var $container = $(this);
+         var $children = $container.children();
+
+         var text;
+         var textLength;
+         var visibleText;
+         var hiddenText;
+
+         if ($children.length) {
+            $children.each(function(index) {
+               var $child = $(this);
+
+               if (index === 0) {
+                  text = $child.text();
+                  textLength = text.length;
+                  visibleText = text.substring(0, subLength);
+                  hiddenText = text.substring(hiddenSubStart, textLength);
+
+                  $child
+                     .text(visibleText)
+                     .append($('<i>...</i>'))
+                     .append($('<a class="showToggle" style="margin-left:5px;font-weight:bold;cursor:pointer;">' + toggleText + '</a>'))
+                     .append($('<span class="sr-only">' + hiddenText + '</span>'));
+               } else {
+                  $child
+                     .html($('<span class="sr-only">' + $child.text() + '</span>'));
+               }
+            });
+         } else {
+            text = $container.text();
+            textLength = text.length;
+            visibleText = text.substring(0, subLength);
+            hiddenText = text.substring(hiddenSubStart, textLength);
+
+            $container
+               .text(visibleText)
+               .append($('<a class="showToggle" style="margin-left:5px;font-weight:bold;cursor:pointer;">' + toggleText + '</a>'))
+               .append($('<span class="sr-only">' + hiddenText + '</span>'));
+         }
+      });
+
+      $('.showToggle').click(function() {
+         $(this)
+            .closest('.' + $containers.attr('class'))
+            .find('span')
+            .removeClass('sr-only');
+         $(this)
+            .closest('.' + $containers.attr('class'))
+            .find('i')
+            .remove();
+         $(this).remove();
+      });
+
+      return $containers;
+   }
+}(jQuery));
+
+
+
+
+
 jQuery(document).ready(function($) {
+	$('.notifyMain').addClass('slideDown');
 
-	function maxLength(maxLen)
-	{
-	    return function truncateToNearestSpace(idx, text)
-	    {
-	        // this may chop in the middle of a word
-	        var truncated = text.substr(0, maxLen);
-
-	        if (/[^\s]$/.test(truncated))
-	            return truncated.replace(/\s[^\s]+$/, "");
-	        else
-	            return truncated.trim();
-	    }
-	}
-
-    $('.notifyMain-toggle').click(function(event) {
+    $('.notifyMain-close').click(function(event) {
         event.preventDefault();
-        $(this).closest('.notifyMain').toggleClass('is-expanded');
-        $('.selector').text(maxLength(50));
+        $(this).closest('.notifyMain').addClass('is-closed');
+        
     });
 });

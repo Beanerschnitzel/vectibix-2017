7e999502b29a3ba26d6c5936769aa75960219f74
Eric Boyer <beanerschnitzel@gmail.com>
2017-06-05 06:58:35 -0600

    structure

--- js/front-end/stickyHeader.js
+++ js/front-end/stickyHeader.js
@@ -1,9 +1,12 @@
-$(function() {
+jQuery(document).ready(function($) {
+  var mql = window.matchMedia('(min-width: 768px)');
+
+  function stickyHeader() {
     var $headerHeight = $('.headerMain').outerHeight();
     var $notifyHeight = $('.notifyMain').outerHeight();
     var $mergedHeight = $headerHeight + $notifyHeight;
     var shrinkHeader = 300;
-    $(window).scroll(function() {
+    $(window).on('scroll.mql', function() {
         var scroll = getCurrentScroll();
         if (scroll >= shrinkHeader) {
             $('.headerMain').addClass('is-sticky');
@@ -17,4 +20,53 @@ $(function() {
     function getCurrentScroll() {
         return window.pageYOffset || document.documentElement.scrollTop;
     }
+  }
+
+  function resetIt() {
+        $(window).off('scroll.mql');
+        $('.headerMain').removeClass('is-sticky');
+        $("main").css("padding-top", '');
+  }
+
+  function screenSize(mql) {
+    if (mql.matches) {
+      stickyHeader();
+    } else {
+      resetIt();
+    }
+  }
+
+  // Handle media query 'change' event
+  mql.addListener(screenSize);
+  screenSize(mql);
 });
+
+
+
+
+
+
+
+
+
+
+// $(function() {
+//     var $headerHeight = $('.headerMain').outerHeight();
+//     var $notifyHeight = $('.notifyMain').outerHeight();
+//     var $mergedHeight = $headerHeight + $notifyHeight;
+//     var shrinkHeader = 300;
+//     $(window).scroll(function() {
+//         var scroll = getCurrentScroll();
+//         if (scroll >= shrinkHeader) {
+//             $('.headerMain').addClass('is-sticky');
+//             $("main").css("padding-top", $mergedHeight);
+//         } else {
+//             $('.headerMain').removeClass('is-sticky');
+//             $("main").css("padding-top", '');
+//         }
+//     });
+
+//     function getCurrentScroll() {
+//         return window.pageYOffset || document.documentElement.scrollTop;
+//     }
+// });

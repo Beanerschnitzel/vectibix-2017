54cdb3cf956224b509bd349ce54dac061ad85c7d
ElTigreBoyer <beanerschnitzel@gmail.com>
2017-06-07 14:19:30 -0600

    content/tweaks

--- js/front-end/internalLinkScrollEffect.js
+++ js/front-end/internalLinkScrollEffect.js
@@ -1,38 +1,52 @@
 //scroll effect to any internal anchor links to give visual feedback of where users are in the document and that they have not been taken to another page
-// Select all links with hashes
-jQuery(document).ready(function($) {
-    $('a[href*="#"]')
-        // Remove links that don't actually link to anything
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function(event) {
-            // On-page links
-            if (
-                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-                location.hostname == this.hostname
-            ) {
-                // Figure out element to scroll to
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                // Does a scroll target exist?
-                if (target.length) {
-                    // Only prevent default if animation is actually gonna happen
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000, function() {
-                        // Callback after animation
-                        // Must change focus!
-                        var $target = $(target);
-                        $target.focus();
-                        if ($target.is(":focus")) { // Checking if the target was focused
-                            return false;
-                        } else {
-                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                            $target.focus(); // Set focus again
-                        };
-                    });
-                }
-            }
-        });
+$(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing');
+    });
 });
+
+// Select all links with hashes
+// jQuery(document).ready(function($) {
+    // $('a[href*="#"]')
+    //     // Remove links that don't actually link to anything
+    //     .not('[href="#"]')
+    //     .not('[href="#0"]')
+    //     .click(function(event) {
+    //         // On-page links
+    //         if (
+    //             location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+    //             location.hostname == this.hostname
+    //         ) {
+    //             // Figure out element to scroll to
+    //             var target = $(this.hash);
+    //             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //             // Does a scroll target exist?
+    //             if (target.length) {
+    //                 // Only prevent default if animation is actually gonna happen
+    //                 event.preventDefault();
+    //                 $('html, body').animate({
+    //                     scrollTop: target.offset().top
+    //                 }, 1000, function() {
+    //                     // Callback after animation
+    //                     // Must change focus!
+    //                     var $target = $(target);
+    //                     $target.focus();
+    //                     if ($target.is(":focus")) { // Checking if the target was focused
+    //                         return false;
+    //                     } else {
+    //                         $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+    //                         $target.focus(); // Set focus again
+    //                     };
+    //                 });
+    //             }
+    //         }
+    //     });
+
+// });
